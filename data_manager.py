594c6fb95f0e5ff5bfba32a57c88456f3e03a3df
melumilic <melumilic@gmail.com>
2022-08-07 01:10:58 -0700

    Add function to edit lowestPrice

--- data_manager.py
+++ data_manager.py
@@ -13,32 +13,40 @@ class DataManager:
         self.__SHEETY_AUTH_TOKEN = os.environ["SHEETY_AUTH_TOKEN"]
         self.__SHEETY_HEADER = {
             "Authorization": "Bearer " + self.__SHEETY_AUTH_TOKEN,
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
         pass
 
-    def add_entry(self, city:String):
-        data = {"price": {"city": city,"iataCode":FlightSearch().get_iata_code(city)}}
+    def add_entry(self, city: String):
+        data = {"price": {"city": city, "iataCode": FlightSearch().get_iata_code(city)}}
         request = requests.post(
             url=self.__SHEETY_API_ENDPOINT, json=data, headers=self.__SHEETY_HEADER
         )
         print(request.text)
         pass
-    
+
     def get_sheet(self) -> Dict:
-        request = requests.get(url=self.__SHEETY_API_ENDPOINT,headers=self.__SHEETY_HEADER)
+        request = requests.get(
+            url=self.__SHEETY_API_ENDPOINT, headers=self.__SHEETY_HEADER
+        )
         print(request.text)
         sheet_json = request.json()
         print(sheet_json)
         return sheet_json
 
-    def update_entry(
-        self,
-    ):
-        pass
+    def update_entry_price(self, index: int, price=None):
+        if price is None:
+            return
+        update_data = {"price": {"lowestPrice": price}}
+
+        request = requests.put(
+            url=self.__SHEETY_API_ENDPOINT + f"/{index}",
+            json=update_data,
+            headers=self.__SHEETY_HEADER,
+        )
+        print(request.text)
 
     def delete_entry(self):
         pass
-
 
     # This class is responsible for talking to the Google Sheet.
--- main.py
+++ main.py
@@ -13,9 +13,15 @@ import os
 # flight_search_test = FlightSearch()
 # print(flight_search_test.get_iata_code("London"))
 
-# data_manager = DataManager()
-# data_manager.add_entry(city="Reno")
-
+data_manager = DataManager()
+data_manager.add_entry(city="Taipei")
+flight_list = data_manager.get_sheet()
+with open("flight_list.json","w") as f:
+    json.dump(flight_list)
+with open("flight_list.json") as f:
+    flight_list = f
+print(flight_list)
+#data_manager.update_entry(index=2,iata_code=FlightSearch().get_iata_code("TPE"))
 # FlightSearch().get_flights(
 #     iata_code="LAX",
 #     destination_airport="RNO",
@@ -28,10 +34,11 @@ import os
 
 #print(search_json["data"][0]["price"])
 
-search_result = FlightSearch().get_cheapest_flight(
-    iata_code="LAX",
-    destination_airport="RNO",
-    date_from="08/08/2022",
-    date_to="02/09/2022",
-)
-print(search_result)
+# search_result = FlightSearch().get_cheapest_flight(
+#     iata_code="LAX",
+#     destination_airport="RNO",
+#     date_from="08/08/2022",
+#     date_to="02/09/2022",
+# )
+
+# print(search_result)
